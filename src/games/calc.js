c556eb29f094d7dcc35dbd25090706dee259ee03
llyt <anton@lytkin.me>
2019-07-20 13:17:51 +0300

    impovements in 'calc' and 'progress'

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,16 +3,12 @@ import engine from '..';
 import genRandInt from '../randomInt';
 
 const description = 'What is the result of the expression?';
-
-const genRandOperation = () => {
-  const operations = '+-*/';
-  return operations[genRandInt(0, operations.length - 1)];
-};
+const operations = '+-*/';
 
 const generateQuestionAnswer = () => {
   const num1 = genRandInt(20, 50);
   const num2 = genRandInt(1, 10);
-  const operation = genRandOperation();
+  const operation = operations[genRandInt(0, operations.length - 1)];
   const question = `${num1} ${operation} ${num2}`;
   let answer;
   switch (operation) {
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,31 +1,39 @@
-import { cons, car, cdr } from '@hexlet/pairs';
+import { cons } from '@hexlet/pairs';
 import engine from '..';
 import genRandInt from '../randomInt';
 
 const description = 'What number is missing in the progression?';
 const progressionLength = 12;
 
-const getProgressionPair = (start, length, diff, missingPosition) => {
-  let missingElement;
-  let result = '';
+const getProgression = (start, length, diff, missingPosition) => {
+  let progression = '';
   for (let i = 0; i < length; i += 1) {
     let current = start + diff * i;
     if (i === missingPosition) {
-      missingElement = current;
       current = '..';
     }
-    result = `${result} ${current}`;
+    progression = `${progression} ${current}`;
   }
-  return cons(result, missingElement);
+  return progression;
+};
+
+const getMissingElement = (start, length, diff, missingPosition) => {
+  let missingElement;
+  for (let i = 0; i < length; i += 1) {
+    const current = start + diff * i;
+    if (i === missingPosition) {
+      missingElement = current;
+    }
+  }
+  return missingElement;
 };
 
 const generateQuestionAnswer = () => {
   const start = genRandInt(1, 100);
   const diffrence = genRandInt(1, 10);
   const missingPosition = genRandInt(0, progressionLength - 1);
-  const progression = getProgressionPair(start, progressionLength, diffrence, missingPosition);
-  const question = car(progression);
-  const answer = cdr(progression);
+  const question = getProgression(start, progressionLength, diffrence, missingPosition);
+  const answer = getMissingElement(start, progressionLength, diffrence, missingPosition);
   return cons(question, String(answer));
 };
 
